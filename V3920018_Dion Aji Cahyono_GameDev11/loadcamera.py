3208f92092d331938ce541f958c5b7615f8a72ec
Dion-aji <89619671+Dion-aji@users.noreply.github.com>
2021-11-17 20:55:54 +0700

    Update loadcamera.py

--- V3920018_Dion Aji Cahyono_GameDev11/loadcamera.py
+++ V3920018_Dion Aji Cahyono_GameDev11/loadcamera.py
@@ -1,9 +1,9 @@
-from math import pi, sin, cos  #untuk menghitung pergerakan kamera
-from direct.showbase.ShowBase import ShowBase #untuk mengambil dan menampilkan gambar
-from direct.task import Task #untuk manajemen fungsi
-from direct.actor.Actor import Actor  #meload kelas aktor 
-from direct.interval.IntervalGlobal import Sequence #durasi movement 
-from panda3d.core import Point3  #untuk mengatur koordinat 
+from math import pi, sin, cos 
+from direct.showbase.ShowBase import ShowBase 
+from direct.task import Task 
+from direct.actor.Actor import Actor  
+from direct.interval.IntervalGlobal import Sequence 
+from panda3d.core import Point3  
 from panda3d.core import ClockObject
 
 keyMap = {
@@ -18,32 +18,27 @@ def updateKeyMap(key, state):
     keyMap[key] = state
 class MyApp(ShowBase):
     def __init__(self):
-        ShowBase.__init__(self)  # menginisialisasi modul ShowBase
+        ShowBase.__init__(self) 
 
-        # Nonaktifkan kontrol trackball kamera.
+      
         self.disableMouse()
 
-        # memuat environment model.
+        
         self.scene = self.loader.loadModel("models/environment")
-        # Atur ulang model yang akan dirender.
+        
         self.scene.reparentTo(self.render)
-        # Terapkan transformasi skala dan posisi pada model.
         self.scene.setScale(0.25, 0.25, 0.25)
         self.scene.setPos(-8, 42, 0)
-
-        # Tambahkan prosedur spinCameraTask ke pengelola tugas.
         self.taskMgr.add(self.spinCameraTask, "SpinCameraTask")
 
-        # Load dan ubah aktor panda.
+        # Load pada panda.
         self.Panda = Actor("models/panda-model",
                            {"walk": "models/panda-walk4"})
         self.Panda.setScale(0.005, 0.005, 0.005)
         self.Panda.reparentTo(self.render)
-
-        # Agar 3d model panda dapat berjalan
         self.Panda.loop("walk")
 
-        # Membuat event sebagai arah yang akan ditekan melalui keyboard
+      
         self.accept("arrow_left", updateKeyMap, ["left", True])
         self.accept("arrow_left-up", updateKeyMap, ["left", False])
 
@@ -58,14 +53,10 @@ class MyApp(ShowBase):
 
         self.accept("space", updateKeyMap, ["rotate", True])
         self.accept("space-up", updateKeyMap, ["rotate", False])
-
-        # Memberi nilai Kecepatan serta arah
         self.speed=6
-        self.angle=0  # secara default bernilai 0
+        self.angle=0  
 
         self.taskMgr.add(self.update, "update")
-
-    # Tentukan prosedur untuk memindahkan kamera.
     def spinCameraTask(self, task):
         angleDegrees=task.time * 6.0
         angleRadians=angleDegrees * (pi / 180.0)
@@ -80,8 +71,6 @@ class MyApp(ShowBase):
         dt = globalClock.getDt()
 
         pos = self.Panda.getPos()
-
-        # Membuat beberapa pengaturan action sesuai keyMap 
         if keyMap["left"]:
             pos.x -= self.speed * dt
         if keyMap["right"]:
@@ -97,15 +86,11 @@ class MyApp(ShowBase):
         self.Panda.setPos(pos)
 
         return task.cont
-# inisialisasi Function MyApp() ke variabel app
+
 app=MyApp()
 # load Musik
 mySound=app.loader.loadSfx("Sparkle.ogg")
-# mutar Musik
 mySound.play()
-# Membuat Musik terus berulang
 mySound.setLoop(True)
-# Mengatur volume
 mySound.setVolume(13)
-# menjalankan aplikasi
 app.run()
